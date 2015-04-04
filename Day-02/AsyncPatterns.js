00e10dda588c8c8a71bafd5daf646d1e98bb1e88
tkmagesh <tkmagesh77@gmail.com>
2015-04-04 12:48:58 +0530

    Server Communication

--- Day-02/AsyncPatterns.js
+++ Day-02/AsyncPatterns.js
@@ -10,7 +10,7 @@ function usingAddSync(x,y){
     console.log("result = ", result);
 }
 
-//Async
+//Async - Using callbacks
 function addAsync(x,y, onResult){
     setTimeout(function(){
         console.log("returning result");
@@ -26,3 +26,66 @@ function usingAddAsync(x,y){
         console.log("result = ", result);
     });
 }
+
+//Async - using events
+function getAdder(){
+    var handlers = [];
+
+    function triggerResult(result){
+        handlers.forEach(function(handler){
+            handler(result);
+        });
+    }
+
+    return {
+        add : function(x,y){
+            setTimeout(function(){
+                console.log("returning result");
+                var result = x + y;
+                triggerResult(result);
+            },5000);
+        },
+        addSubscriber : function(subscriptionFn){
+            handlers.push(subscriptionFn);
+        }
+    }
+
+}
+
+//using add(events)
+var adder = getAdder();
+adder.add(100,200);
+adder.addSubscriber(function(result){
+        console.log("result = ", result);
+    });
+
+
+//Async - Promises
+function addAsyncUsingPromise(x,y){
+    var p = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            console.log("returning result");
+            var result = x + y;
+            resolve(result);
+        },5000);
+    });
+    return p;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
